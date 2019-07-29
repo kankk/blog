0c22763f950b958f41949dd2255f7a4d3a8e48d2
KanKK <286454796@qq.com>
2019-07-29 10:05:36 +0800

    feat: Error页面

---
+++ layouts/error.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="error-page">
+    <div class="error-content">
+      <span class="error-content-code">{{ error.statusCode }}</span>
+      <nuxt-link class="error-content-home" to="/" tag="span">
+        首页
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted () {
+    console.log(this.error)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/css/color.scss';
+.error-page {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .error-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    z-index: 2;
+
+    .error-content-code {
+      font-size: 48px;
+      color: $color-text;
+      font-weight: 800;
+    }
+
+    .error-content-home {
+      font-size: 15px;
+      color: $color-sub-text;
+      &:hover {
+        cursor: pointer;
+        color: $color-main;
+      }
+    }
+  }
+}
+</style>
